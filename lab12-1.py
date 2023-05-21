b5c592d1ea3a125c229194f7ce190d1d72fd0425
Puzanov Alexandr <leifon2011@gmail.com>
2023-05-22 00:44:52 +0300

    fiks

--- lab12-1.py
+++ lab12-1.py
@@ -1,6 +1,23 @@
 from tkinter import *
-
+from tkinter import ttk
+from tkinter.messagebox import showerror, showwarning, showinfo
 root = Tk()
+
+def btn_clicky():
+    info_str ='Ванильное'
+    showinfo(title='Вид мороженки', message=info_str)
+    
+def btn_clickb():
+    info_str1 ='Шоколадное'
+    showinfo(title='Вид мороженки', message=info_str1) 
+    
+def btn_clickbl():
+    info_str2 ='Ягодное'
+    showinfo(title='Вид мороженки', message=info_str2)
+    
+def btn_clickg():
+    info_str3 ='Мятное'
+    showinfo(title='Вид мороженки', message=info_str3)  
 
 root['bg'] = '#000000'
 root.title('Кафе-мороженное')
@@ -17,15 +34,15 @@ frame.place(relwidth=1, relheight=1)
 
 title = Label(frame, text='Виды мороженного', bg='gray', font=40)
 title.pack()
-btn = Button(frame, text='Vanille', bg='yellow')
+btn = Button(frame, text='Vanille', bg='yellow', command=btn_clicky)
 btn.pack()
 
-btn = Button(frame, text='Chocolate', bg='brown')
+btn = Button(frame, text='Chocolate', bg='brown',command=btn_clickb)
 btn.pack()
 
-btn = Button(frame, text='Strawberry', bg='blue')
+btn = Button(frame, text='Strawberry', bg='blue',command=btn_clickbl)
 btn.pack()
 
-btn = Button(frame, text='Mint', bg='green')
+btn = Button(frame, text='Mint', bg='green',command=btn_clickg)
 btn.pack()
 root.mainloop()
