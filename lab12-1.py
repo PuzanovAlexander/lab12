88c02da1e5c81589091c5f31e4c135119a2a359c
Puzanov Alexandr <leifon2011@gmail.com>
2023-05-22 00:10:52 +0300

    ficks

--- lab12-1.py
+++ lab12-1.py
@@ -2,22 +2,30 @@ from tkinter import *
 
 root = Tk()
 
-root['bg'] = '#fafafa'
-root.title('название программы')
-root.wm_attributes('-alpha', 0.7)
-root.geometry('450x400')
+root['bg'] = '#000000'
+root.title('Кафе-мороженное')
+root.wm_attributes('-alpha', 1)
+root.geometry('200x200')
 
 root.resizable(width=False, height=False)
 
-canvas = Canvas (root, height=450, width=400)
+canvas = Canvas (root, height=200, width=200)
 canvas.pack()
 
 frame = Frame(root, bg='red')
 frame.place(relwidth=1, relheight=1)
 
-title = Label(frame, text='Текст подсказка', bg='gray', font=40)
+title = Label(frame, text='Виды мороженного', bg='gray', font=40)
 title.pack()
-btn = Button(frame, text='Кнопка', bg='yellow')
+btn = Button(frame, text='Vanille', bg='yellow')
 btn.pack()
 
+btn = Button(frame, text='Chocolate', bg='brown')
+btn.pack()
+
+btn = Button(frame, text='Strawberry', bg='blue')
+btn.pack()
+
+btn = Button(frame, text='Mint', bg='green')
+btn.pack()
 root.mainloop()
